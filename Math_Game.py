d9833840e4ada624dacf5c2f521bc3a3cba067fb
Russell Chubb <Russell.d.chubb@gmail.com>
2025-08-27 09:35:57 +1200

    added backlog items

--- Math_Game.py
+++ Math_Game.py
@@ -4,6 +4,29 @@ import math
 import os
 import random
 
+# --- To do ---
+'''
+1) Fix bug where the scores aren't correctly being written to the .txt file - Getting weird edge case where sometimes I get a completly different high-scores board. Weird behaviour, I just need it to save each completed attempt to the .txt file, and read the .txt file to the screen.
+2) Get theming consistent on the different UI elements (buttons, backgrounds, etc.)
+3) Move clock to somewhere where people can see the time remaining more easily? (Not sure how I'd go about this, open to ideas)
+4) Implement hot streak (if you get 3 in a row, you get a custom sound and maybe some kind of fire effect?)
+5) Add a back button to the high-scores screen.
+6) Add two different background music tracks, one for the game-state, and one for the menu.
+7) Add that Minecraft style random text in the top right hand corner of the menu
+8) Add a settings menu where you can change music volume, SFX Volume, time limit, number range, etc.
+9) Structure the folder (and the git repo, more appropriately)
+10) Figure out how to run tests on the file (linting etc)
+11) Create custom .ico file
+12) Create logo for the game + Draw the logo on the main-screen.
+13) Fix the fade effect, it currently doesn't work as intended. Also, I'd love more of a pixelated fade rather than a clean fade, as it's more in style with the game.
+14) Make "Score" larger, (and maybe glow) on the finished game scene (so that people better understand what they achieved)
+15) Add a line cursor to the text input field in the game state as well as the enter a name menu once the game has finished.
+16) Add in the count-down beep, and then the final buzzer sound.
+
+MISC: Where's Russell Game?
+
+'''
+
 # --- Sound Setup ---
 import os
 import pygame
@@ -11,7 +34,7 @@ import pygame
 # --- Sound Setup ---
 pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
 
-# Absolute path to your sound folder
+# Sound Folder Path
 BASE_PATH = r"C:\Users\rc30\OneDrive - Meridian Energy Limited\Documents\Programming\Personal\Math_Game"
 
 def load_sound(filename):
